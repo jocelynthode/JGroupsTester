a1c5ac37aeadcf7454d11ed69d70abd175e527a8
samtdr <samtdr@yahoo.com>
2016-06-15 09:13:14 +0000

    fix interface

--- scripts/container-start-script.sh
+++ scripts/container-start-script.sh
@@ -17,5 +17,5 @@ MY_NAME=(${TMP//./ })
 sleep 1m
 
 echo 'Starting epto peer: starting java' | addtime > localhost.txt 2>&1
-exec java -Xms50m -Xmx250m -Djava.net.preferIPv4Stack=true -cp ../build/libs/jgroups-tester-1.0-SNAPSHOT-all.jar EventTesterKt | addtime >> localhost.txt 2>&1
+exec java -Xms50m -Xmx250m -Djgroups.bind_addr=$MY_IP_ADDR -Djava.net.preferIPv4Stack=true -cp ../build/libs/jgroups-tester-1.0-SNAPSHOT-all.jar EventTesterKt | addtime >> localhost.txt 2>&1
 
