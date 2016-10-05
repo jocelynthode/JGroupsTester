cce5a0f2196aecf704c52a04bd9a9fd4386a017d
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-05 17:29:56 +0200

    Add Peer number

--- scripts/container-start-script.sh
+++ scripts/container-start-script.sh
@@ -1,16 +1,7 @@
 #!/usr/bin/env bash
 # This script needs to run in the container
-
-addtime() {
-    while IFS= read -r line; do
-        echo "$(date +%s%N | cut -b1-13) $line"
-    done
-}
-
 MY_IP_ADDR=$(/bin/hostname -I)
 
-# wait for all peers
-sleep 20s
 echo 'Starting jgroup peer'
 echo "${MY_IP_ADDR}"
 echo "${PEER_NUMBER}"
@@ -18,5 +9,5 @@ echo "${FILENAME}"
 MY_IP_ADDR=($MY_IP_ADDR)
 echo "${MY_IP_ADDR[0]}"
 exec java -Xms150m -Xmx330m -Djgroups.bind_addr="${MY_IP_ADDR[0]}" -Djava.net.preferIPv4Stack=true \
- -cp ./jgroups-tester-1.0-SNAPSHOT-all.jar EventTesterKt | addtime > "/data/${MY_IP_ADDR[0]}_${FILENAME}.txt" 2>&1
+ -cp ./jgroups-tester-1.0-SNAPSHOT-all.jar EventTesterKt "$PEER_NUMBER" > "/data/${MY_IP_ADDR[0]}_${FILENAME}.txt" 2>&1
 ls
