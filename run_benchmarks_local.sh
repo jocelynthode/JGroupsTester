28dd83bc10b35f0be79a7f7449d4112a800d1bb7
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-13 17:03:24 +0200

    Fix local benchmark script

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -14,15 +14,15 @@ echo "START..."
 ./gradlew docker
 
 # Clean everything at Ctrl+C
-trap 'docker service rm epto-service && docker service rm epto-tracker && exit' TERM INT
+trap 'docker rm -f $(docker ps -aqf ancestor=jgroups) && exit' TERM INT
 
 for i in $(seq 1 $PEER_NUMBER); do docker run --network host -d --env "FILENAME=${i}" -m 250m \
---env "PEER_NUMBER=$PEER_NUMBER" -v /home/debian/data:/data jgroups; done
+--env "PEER_NUMBER=$PEER_NUMBER" -v /home/jocelyn/tmp/data:/data jgroups; done
 
 echo "Running JGroups tester..."
 sleep 1m
 
-docker rm -f $(docker ps -aqf ancestor=swarm-m:5000/jgroups)
+docker rm -f $(docker ps -aqf ancestor=jgroups)
 echo "Done"
 
 
