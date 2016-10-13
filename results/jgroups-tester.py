b01a1a3cdc2505a23e79b95fb2ffe5a82c1cb1de
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-13 18:11:04 +0200

    Add messages sent/received to the log parser

--- results/jgroups-tester.py
+++ results/jgroups-tester.py
@@ -2,11 +2,15 @@
 import re
 import sys
 from pathlib import Path
+from itertools import islice
+from collections import namedtuple
 
 length = 0
 
+Stats = namedtuple('Stats', ['duration', 'sent', 'received', 'sent_bytes', 'received_bytes'])
 
-def extract_duration(lines):
+
+def extract_stats(lines):
     it = iter(lines)  # Force re-use of same iterator
     end_at = 0
     start_at = 0
@@ -20,18 +24,26 @@ def extract_duration(lines):
         if match:
             end_at = int(match.group(1))
             break
-    return end_at - start_at
+
+    def extract_int(a_line):
+        return int(re.findall(r' [0-9]+$', a_line)[0])
+
+    sent, received, sent_bytes, received_bytes = (
+        extract_int(line) for line in islice(it, 0, 4)
+    )
+
+    return Stats(end_at - start_at, sent, received, sent_bytes, received_bytes)
 
 
-def all_durations():
+def all_stats():
     for fpath in Path().glob(sys.argv[1]+'/**/172.*.txt'):
         global length
         length += 1
         with fpath.open() as f:
-            yield extract_duration(f)
+            yield extract_stats(f)
 
-
-durations = list(all_durations())
+stats = list(all_stats())
+durations = [stat.duration for stat in stats]
 mininum = min(durations)
 maximum = max(durations)
 average = sum(durations) / len(durations)
@@ -42,3 +54,7 @@ print("------------------------")
 print("Least time to deliver: %d ms" % mininum)
 print("Most time to deliver: %d ms" % maximum)
 print("Average time to deliver: %d ms" % average)
+print("Total number of messages sent: %d" % sum([stat.sent for stat in stats]))
+print("Total number of bytes sent: %d" % sum([stat.sent_bytes for stat in stats]))
+print("Total number of messages received: %d" % sum([stat.received for stat in stats]))
+print("Total number of bytes received: %d" % sum([stat.received_bytes for stat in stats]))
