3e66d1b218862d4febe1872b8edfa69283e8c759
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-25 14:24:28 +0200

    Use services

--- run_benchmarks_local.sh
+++ run_benchmarks_local.sh
@@ -3,6 +3,7 @@
 
 MANAGER_IP=172.16.2.119
 PEER_NUMBER=$1
+TIME_ADD=$2
 
 if [ -z "$PEER_NUMBER" ]
   then
@@ -10,20 +11,49 @@ if [ -z "$PEER_NUMBER" ]
     exit
 fi
 
+if [ -z "$TIME_ADD" ]
+  then
+    echo "you have to indicate by how much you want to delay JGroups start"
+    exit
+fi
+
 echo "START..."
 ./gradlew docker
 
 # Clean everything at Ctrl+C
-trap 'docker rm -f $(docker ps -aqf ancestor=jgroups) && exit' TERM INT
+trap 'docker service rm jgroups-service; docker service rm jgroups-tracker; exit' TERM INT
 
-for i in $(seq 1 $PEER_NUMBER); do docker run --network host -d --env "FILENAME=${i}" -m 250m \
---env "PEER_NUMBER=$PEER_NUMBER" -v /home/jocelyn/tmp/data:/data jgroups; done
+docker swarm init && docker network create -d overlay --subnet=172.110.0.0/16 jgroups_network
 
+docker service create --name jgroups-tracker --network jgroups_network --replicas 1 \
+--constraint 'node.role == manager' --limit-memory 250m jgroups-tracker:latest
+
+until docker service ls | grep "1/1"
+do
+    sleep 1s
+done
+
+TIME=$(( $(date +%s%3N) + $TIME_ADD ))
+docker service create --name jgroups-service --network jgroups_network --replicas ${PEER_NUMBER} \
+--env "PEER_NUMBER=${PEER_NUMBER}" --env "TIME=$TIME" \
+--limit-memory 250m --log-driver=journald --restart-condition=none \
+--mount type=bind,source=/home/jocelyn/tmp/data,target=/data jgroups:latest
+
+while docker service ls | grep " 0/$PEER_NUMBER"
+do
+    sleep 1s
+done
 echo "Running JGroups tester..."
-sleep 1m
+# wait for service to end
+until docker service ls | grep -q " 0/$PEER_NUMBER"
+do
+    sleep 5s
+done
 
-docker rm -f $(docker ps -aqf ancestor=jgroups)
-echo "Done"
+docker service rm jgroups-tracker
+docker service rm jgroups-service
+
+echo "Services removed"
 
 
 
