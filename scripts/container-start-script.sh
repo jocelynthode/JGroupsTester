0422ab76db4afa2dbfb5294e8a990bc6eabf3f40
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-10-25 14:26:48 +0200

    Pass GossipRouter ip to JGroups peers

--- scripts/container-start-script.sh
+++ scripts/container-start-script.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 # This script needs to run in the container
 MY_IP_ADDR=$(/bin/hostname -I)
-
+TRACKER_IP=$(dig +short jgroups-tracker)
 echo 'Starting jgroup peer'
 echo "${MY_IP_ADDR}"
 echo "${PEER_NUMBER}"
-echo "$PROTOCOL_STACK"
 echo "$TIME"
 MY_IP_ADDR=($MY_IP_ADDR)
 echo "${MY_IP_ADDR[0]}"
+echo "${TRACKER_IP}[12001]"
 
 dstat -n -N eth0 --output "/data/capture/${MY_IP_ADDR[0]}.csv" &
 dstat_pid=$!
-exec java -Xms100m -Xmx210m -Djgroups.bind_addr="${MY_IP_ADDR[0]}" -Djava.net.preferIPv4Stack=true \
--Djgroups.tcpping.initial_hosts="172.110.0.3[43568],172.110.0.4[43568],172.110.0.5[43568],172.110.0.6[43568],172.110.0.7[43568]" \
--cp ./jgroups-tester-1.0-SNAPSHOT-all.jar -Dlogfile.name="${MY_IP_ADDR[0]}" EventTesterKt "$PEER_NUMBER" \
-"$(cat /host_etc/hostname).xml"  "$TIME"
+exec java -Xms100m -Xmx210m -Djgroups.bind_addr="${MY_IP_ADDR[0]}" \
+-Djgroups.tunnel.gossip_router_hosts="${TRACKER_IP}[12001]" -Djava.net.preferIPv4Stack=true \
+-cp ./jgroups-tester-1.0-SNAPSHOT-all.jar -Dlogfile.name="${MY_IP_ADDR[0]}" EventTesterKt "$PEER_NUMBER" "$TIME"
 kill ${dstat_pid}
