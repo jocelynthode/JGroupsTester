4489eb5479b08e76b84b1024813af11aa1bbe171
Jocelyn Thode <jocelyn.thode@gmail.com>
2016-11-11 18:53:17 +0100

    Add possibility to pass fixed_rate

--- scripts/container-start-script.sh
+++ scripts/container-start-script.sh
@@ -11,11 +11,12 @@ echo "${MY_IP_ADDR[0]}"
 echo "${TRACKER_IP}[12001]"
 echo "${EVENTS_TO_SEND}"
 echo "${RATE}"
+echo "${FIXED_RATE}"
 
 dstat -n -N eth0 --output "/data/capture/${MY_IP_ADDR[0]}.csv" &
 dstat_pid=$!
-java -Xms100m -Xmx260m -Djgroups.bind_addr="${MY_IP_ADDR[0]}" \
+exec java -Xms100m -Xmx260m -Djgroups.bind_addr="${MY_IP_ADDR[0]}" \
 -Djgroups.tunnel.gossip_router_hosts="${TRACKER_IP}[12001]" -Djava.net.preferIPv4Stack=true \
 -cp ./jgroups-tester-1.0-SNAPSHOT-all.jar -Dlogfile.name="${MY_IP_ADDR[0]}" EventTesterKt --events "$EVENTS_TO_SEND" \
---rate "$RATE" --fixed-rate 50 "$PEER_NUMBER" "$TIME"
+--rate "$RATE" --fixed-rate "$FIXED_RATE" "$PEER_NUMBER" "$TIME"
 kill ${dstat_pid}
